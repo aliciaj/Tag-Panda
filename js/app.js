11bf1e3bacc80e37a46487ecc288b8084102ded8
Alicia Brooks <abrooks@Alicias-MacBook-Pro.local>
2014-08-15 17:25:01 -0400

    Super basic start to Tag Panda dashboard

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$( window ).load(function() {
+    $(".columns").height($(".app-body").parent().height()); 
+}
