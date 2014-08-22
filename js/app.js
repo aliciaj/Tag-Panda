e072db74d9d0df46aa910740da52475ea8f4247f
Alicia Brooks <abrooks@Alicias-MacBook-Pro.local>
2014-08-22 16:26:18 -0400

    Starting to add sidebar content

--- js/app.js
+++ js/app.js
@@ -6,11 +6,13 @@ $(document).foundation();
 function mediaqueryresponse(mql){
     if (mql.matches){ // if media query matches
         // console.log("The condition " + mql.media + " has been met")
-        $(".columns").height($(".app-body").parent().height() - $('.top-bar').height());
+        $(".full-height").height($(".app-body").parent().height() - $('.top-bar').height());
+        // console.log('on load');
+        // console.log($(".app-body").parent().height() - $('.top-bar').height());
     }
     else{
         // console.log("Condition not met yet")
-        $(".columns").height() == 'auto';
+        $(".full-height").height() == 'auto';
     }
 }
 
@@ -23,9 +25,15 @@ $( window ).load(function() {
 });
 
 
-// On Resize
-$( window ).resize(function() {
+//On Resize
+// $( window ).resize(function() {
+$(window).on('resize', function(e){
+// $(window).on('resize', Foundation.utils.throttle(function(e){
     var mql = window.matchMedia("screen and (min-width: 64.063em)")
     mediaqueryresponse(mql) 
     mql.addListener(mediaqueryresponse)
+
+    // console.log($(".app-body").parent().height() - $('.top-bar').height());
+    console.log($(window).height());
 });
+// }, 300));
