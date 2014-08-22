d461814a14fe47a9db63afe36a329d92a0e98842
Alicia Brooks <abrooks@Alicias-MacBook-Pro.local>
2014-08-22 13:23:07 -0400

    Media query match classic way

--- js/app.js
+++ js/app.js
@@ -2,13 +2,30 @@
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
 
-$( window ).load(function() {
-    $(".columns").height($(".app-body").parent().height() - $('.top-bar').height()); 
-    console.log($(".app-body").parent());
 
-    
+function mediaqueryresponse(mql){
+    if (mql.matches){ // if media query matches
+        // console.log("The condition " + mql.media + " has been met")
+        $(".columns").height($(".app-body").parent().height() - $('.top-bar').height());
+    }
+    else{
+        // console.log("Condition not met yet")
+        $(".columns").height() == 'auto';
+    }
+}
+
+// On Load
+$( window ).load(function() {
+
+    var mql = window.matchMedia("screen and (min-width: 64.063em)")
+    mediaqueryresponse(mql)
+    mql.addListener(mediaqueryresponse)
 });
 
+
+// On Resize
 $( window ).resize(function() {
-  $(".columns").height($(".app-body").parent().height() - $('.top-bar').height()); 
+    var mql = window.matchMedia("screen and (min-width: 64.063em)")
+    mediaqueryresponse(mql) 
+    mql.addListener(mediaqueryresponse)
 });
