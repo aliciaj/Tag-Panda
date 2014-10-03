9390a9c5465b223eddc9c7ec9b121b6dd962448a
Alicia Brooks <abrooks@rmunoz-7lptp.amag.local>
2014-10-03 10:59:56 -0400

    New structure

---
+++ app.php
@@ -0,0 +1,147 @@
+<!doctype html>
+<html class="no-js" lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>Tag Panda</title>
+		<link rel="stylesheet" href="stylesheets/app.css" />
+		<link rel="stylesheet" href="stylesheets/foundation-icons.css" />
+		<script src="bower_components/modernizr/modernizr.js"></script>
+
+		<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+	    <link rel="shortcut icon" type="image/x-icon" href="favicon.png" />
+
+	</head>
+	<body>
+
+		<div class="site-wrapper clearfix">
+			
+			<nav class="top-bar" data-topbar>
+				<ul class="title-area">
+					<li class="name">
+						<h1><a href="/">Tag Panda</a></h1>
+			    	</li>
+			  	</ul>
+
+			  	<section class="top-bar-section">
+			    	<!-- Right Nav Section -->
+			    	<ul class="right">
+			      		<li class="has-dropdown">
+			        		<a href="#">Alicia Brooks</a>
+					        <ul class="dropdown">
+					          	<li><a href="#">View Profile</a></li>
+					          	<li><a href="#">Settings</a></li>
+					        </ul>
+			      		</li>
+				    </ul>
+
+			  	</section>
+			</nav>
+
+			<!-- Main Content -->
+			<div class="grid-row display app-body clearfix">
+				
+				<!-- TAG COLUMN -->
+				<div class="large-3 columns sidebar full-height">
+					<h1>Your Shoots</h1>
+						<ul class="list">
+							<li><a href="">All Items</a></li>
+						</ul>
+
+						<form action="" class="clearfix">
+							<div class="row">
+								<div class="large-6 columns">
+							      <div class="row collapse postfix-round">
+							        <div class="small-9 columns">
+							          <input type="text" placeholder="Value">
+							        </div>
+							        <div class="small-3 columns">
+							          <a href="#" class="button postfix">Go</a>
+							        </div>
+							      </div>
+							    </div>
+						    </div>
+						</form>
+
+					<h1>Tags</h1>
+						<ul class="tags">
+							<li><a class="" href=""><span class="label round">CSS</span></a></li>
+							<li><a class="button tiny round" href="">HTML</a></li>
+							<li><a class="button tiny round" href="">Foundation</a></li>
+							<li><a class="button tiny round" href="">Inspiration</a></li>
+							<li><a class="button tiny round" href="">Yoga</a></li>
+							<li><a class="button tiny round" href="">Red Pandas</a></li>
+							<li><a class="button tiny round" href="">CSS</a></li>
+							<li><a class="button tiny round" href="">HTML</a></li>
+							<li><a class="button tiny round" href="">Foundation</a></li>
+							<li><a class="button tiny round" href="">Inspiration</a></li>
+							<li><a class="button tiny round" href="">Yoga</a></li>
+							<li><a class="button tiny round" href="">Red Pandas</a></li>
+						</ul>
+				</div>
+
+				<!-- BOOKMARKS COLUMN -->
+				<div class="large-6 medium-8 small-12 columns main-section full-height">
+					<dl class="sub-nav">
+					  <dt>Filter:</dt>
+					  <dd class="active"><a href="#">All</a></dd>
+					  <dd><a href="#">Images</a></dd>
+					  <dd><a href="#">Videos</a></dd>
+					  <dd><a href="#">Pages</a></dd>
+					</dl>
+
+					<h2>Bookmarks</h2>
+
+					<ul class="small-block-grid-1 medium-block-grid-3 large-block-grid-4">
+						<li>
+							<img class="th active" src="http://upload.wikimedia.org/wikipedia/commons/8/82/Giant_Panda_Tai_Shan.JPG">
+							<a href="">linktothisbo...</a>
+						</li>
+
+						<li><img class="th" src="http://img.timeinc.net/time/photoessays/2011/panda_wolong/panda_wolong_01.jpg"></li>
+
+						<li><img class="th" src="http://3.bp.blogspot.com/_CIRBn1Vfd2w/TM4hHdCoRSI/AAAAAAAAAZg/_D7daIVT2rA/s1600/panda01.jpg"></li>
+
+						<li><img class="th" src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQJ8eNMoaeqlsEb92iwLp5RM6c2NAv7ui8GvToTPkeThay1sTFk"></li>
+
+						<li><img class="th" src="http://2.bp.blogspot.com/-nqb9kzno-Zw/UDfIJwWpuqI/AAAAAAAAGVI/d14QAQn2hbs/s1600/3-RED-PANDA-CHINA.JPG"></li>
+
+						<li><img class="th" src="http://news.worldwild.org/wp-content/uploads/2008/09/red_panda.jpg"></li>
+					</ul>
+				</div>
+
+				<!-- Description COLUMN -->
+				<div class="large-3 medium-4 small-12 columns sidebar full-height">
+					<a href=""><i class="fi-pencil right"></i></a>
+					<h1>Title of selected bookmark</h1>
+
+					<a href="">fullurlofbookmark.com</a>
+					
+					<h2>Description</h2>
+					<div class="panel">
+						<p>Description of saved bookmark that user saved</p>
+					</div>
+
+					<h2>Tags</h2>
+					<div class="panel">
+						<ul class="tags clearfix">
+							<li><a class="" href=""><span class="label round">CSS</span></a></li>
+							<li><a class="" href=""><span class="label round">Cute</span></a></li>
+							<li><a class="" href=""><span class="label round">Panda</span></a></li>
+						</ul>
+					</div>
+
+					<p>October 24th, 2015</p>
+
+					<img class="th" src="http://upload.wikimedia.org/wikipedia/commons/8/82/Giant_Panda_Tai_Shan.JPG">
+				</div>
+
+			</div>
+			<!-- End Main Content -->
+		</div>
+
+		<script src="bower_components/jquery/dist/jquery.min.js"></script>
+		<script src="bower_components/foundation/js/foundation.min.js"></script>
+		<script src="js/app.js"></script>
+	</body>
+</html>
--- library/baseModel.php
+++ library/baseModel.php
@@ -1,16 +1,5 @@
 <?php
 class Model extends Database {
-    protected $_model;
-
-    function __construct() {
-
-        // $this->connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
-        // $this->_model = get_class($this);
-        // $this->_table = strtolower($this->_model)."s";
-
-        // find parameter or something
-    }
-
-    function __destruct() {
+    
     }
 }
